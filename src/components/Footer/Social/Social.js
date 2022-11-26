07d907bc966b88e93de346b74294ed7f8e9bf5a3
Nicolas Acevedo <nicoacevedocotiza@gmail.com>
2022-11-26 18:30:53 -0300

    npm

--- src/components/Footer/Social/Social.js
+++ src/components/Footer/Social/Social.js
@@ -6,7 +6,7 @@ export const Social = () => {
         <h5>Comunicate</h5>
         <br />
         <ul>
-            <li><a href=''>Mail</a></li>
+            <li>Mail</li>
             <li><a href='https://www.instagram.com/nico.devcom/'>Instagram</a></li>
             <li><a href='https://www.facebook.com/profile.php?id=100085659564621'>Facebook</a></li>
             <li><a href='https://www.linkedin.com/in/nicolas-acevedo-ecommerce/'>Linkedin</a></li>
